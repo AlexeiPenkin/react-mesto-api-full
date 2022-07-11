1da8533b1acd151a896eed3b03d5ae3bb6529852
Alexei Penkin <iva13293376@yandex.ru>
2022-07-11 21:57:38 +0500

    backend

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const FORBIDDEN_ERROR = require('../errors/forbidden-error');
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET, NODE_ENV } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -15,7 +15,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new FORBIDDEN_ERROR({ message: 'Необходима авторизация' });
   }
